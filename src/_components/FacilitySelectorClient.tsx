16c7db841c6078030ccd1a852f30a51d43e83589
kmdenshi <denshi@komei.jp>
2025-08-13 16:37:58 +0900

    🔨 データを追加

--- src/_components/FacilitySelectorClient.tsx
+++ src/_components/FacilitySelectorClient.tsx
@@ -6,23 +6,29 @@ import MapLoader from '@/_components/MapLoader';
 export default function FacilitySelectorClient() {
   const [selectedFacilityType, setSelectedFacilityType] = useState('asds');
 
-  const handleFacilityChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleFacilityChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
     setSelectedFacilityType(event.target.value);
   };
 
   return (
-    <div className="h-screen w-screen flex flex-col">
-      <div className="p-4 bg-gray-100">
-        <label htmlFor="facility-select" className="mr-2">施設タイプを選択:</label>
+    <div className="flex h-screen w-screen flex-col">
+      <div className="bg-gray-100 p-4">
+        <label htmlFor="facility-select" className="mr-2">
+          施設タイプを選択:
+        </label>
         <select
           id="facility-select"
           value={selectedFacilityType}
           onChange={handleFacilityChange}
-          className="p-2 border rounded"
+          className="rounded border p-2"
         >
           <option value="asds">放課後等デイサービス</option>
+          <option value="sept-a">就労継続支援A</option>
           <option value="sept-b">就労継続支援B</option>
           <option value="pco">計画相談事業所</option>
+          <option value="ccd">障害児相談支援事業所</option>
         </select>
       </div>
       <div className="flex-grow">
